083c9bbeea825d2e3c9d3d10a52d1bd8e624dcd8
deepanshu-QM <cdeepanshu231@gmail.com>
2026-09-13 23:34:16 +0530

    solana_adapter

--- bun-env.d.ts
+++ bun-env.d.ts
@@ -15,3 +15,7 @@ declare module "*.module.css" {
   const classes: { readonly [key: string]: string };
   export = classes;
 }
+
+declare module "*.css";
+
+declare module "@solana/wallet-adapter-react-ui/styles.css";
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 
-import {useState} from "react";
+import React, {useMemo} from 'react';
+import { ConnectionProvider, WalletProvider} from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider, WalletConnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+import '@solana/wallet-adapter-react-ui/styles.css';
 
-export function App(){
-  const [wallets, setWallets] = useState<string[]>([]);
+export function App() {
+  const endpoint = "https://api.mainnet-beta.solana.com";        //can use Your own RPC url from hilus 
   return (
-    <div>
-      <b>
-        <button onClick={function() {
-          if (window.backpack) {
-            setWallets(w => [...w, "backpack"])
-          }
-          if (window.solfare) {
-            setWallets(w => [...w, "solfare"])
-          }
-          if (window.phantom) {
-            setWallets(w => [...w, "phantom"])
-          }
-        }}>Connect with Your wallet</button>
-      </b>
-      {wallets.map(wallet => <button>{wallet}</button>)}
-    </div>
-  )
+    <ConnectionProvider endpoint={endpoint}>
+    <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+            { /* Your app's components go here, nested within the context providers. */ }
+        </WalletModalProvider>
+    </WalletProvider>
+</ConnectionProvider>
+  );
 }
